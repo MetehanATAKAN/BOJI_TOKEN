deab38551b37c3046d182f635d0b65ebd668a1a9
Metehan ATAKAN <m.atakakan@diten.tech>
2022-09-14 00:47:53 +0300

    .

--- src/components/sections/RoadMap.js
+++ src/components/sections/RoadMap.js
@@ -8,79 +8,80 @@ const RoadMap = () => {
     {
       phaseHeader:'Phase 1',
       phaseItem: [
-        '✓ Concept Design        ',
-        '✓ Website Launch        ',
-        '✓ Token Launch ',
-        '✓ Social Media Channels ',
-        '✓ Marketing Strategy ',
-        '✓ Community Building ',
-        '✓ Whitepaper',
+        {name:'✓ Concept Design',completed:true},
+        {name:'✓ Website Launch',completed:true},
+        {name:'✓ Token Launch ',completed:true},
+        {name:'✓ Social Media Channels ',completed:true},
+        {name:'✓ Marketing Strategy ',completed:true},
+        {name:'✓ Community Building ',completed:true},
+        {name:'✓ Whitepaper',completed:true}
       ],
       isPhaseCompleted: true,
     },
     {
       phaseHeader:'Phase 2',
       phaseItem: [
-        '✓ Launch BOJI’s NFT                        ',
-        '✓ RunBOJI Game Launch ',
-        '✓ Tier 4 Exchange Listing ',
-        '✓ Coingecko Listing ',
-        '✓ Coinmarketcap Listing ',
-        '✓ Audit Report ',
-        '✓ Airdrop ',
-        '✓ Whitepaper and Pitchdeck Upgrade ',
-        '✓ PR Strategies ',
-        '✓ Community AMA ',
-        '✓ Social Media Presence on all continents ',
-        '✓ Surprise Event',
+        {name:'✓ Launch BOJI’s NFT',completed:true},
+        {name:'✓ RunBOJI Game Launch ',completed:true},
+        {name:'✓ Tier 4 Exchange Listing ',completed:true},
+        {name:'✓ Coingecko Listing ',completed:true},
+        {name:'✓ Coinmarketcap Listing ',completed:true},
+        {name:'✓ Audit Report ',completed:true},
+        {name:'✓ Airdrop ',completed:true},
+        {name:'✓ PR Strategies ',completed:true},
+        {name:'✓ Community AMA ',completed:true},
+        {name:'✓ Social Media Presence on all continents ',completed:true},
+        {name:'✓ Surprise Event',completed:true}
       ],
       isPhaseCompleted: true,
     },
     {
       phaseHeader:'Phase 3',
       phaseItem: [
-        'Website v2                     ',
-        'Bojilogy Web3 Launch ',
-        'BojiSwap Launch ',
-        'BojiForce NFT Launch ',
-        'Marketing Campaigns ',
-        'Bojilogy Mobile Version Launch ',
-        'Tier 3 Exchange Listing ',
-        'Event For Charity ',
-        'Airdrop ',
+        {name:'✓ Website v2',completed:true},
+        {name:'Pitchdeck Upgrade',completed:false},
+        {name:'Whitepaper v2',completed:false},
+        {name:'BojiSwap Launch ',completed:false},
+        {name:'BojiForce NFT Launch ',completed:false},
+        {name:'Bojilogy Web3 Launch ',completed:false},
+        {name:'Marketing Campaigns ',completed:false},
+        {name:'Bojilogy Mobile Version Launch ',completed:false},
+        {name:'Tier 3 Exchange Listing ',completed:false},
+        {name:'Event For Charity ',completed:false},
+        {name:'Airdrop ',completed:false}
       ],
       isPhaseCompleted: 'continues',
     },
     {
       phaseHeader:'Phase 4',
       phaseItem: [
-        'Audit Report (Certik, Hacken) ',
-        'Tier2 Exchange Listing ',
-        'Runboji v2 ',
-        'Influencer onboarding ',
-        'Bojilogy Version Upgrade ',
-        'Charity Partnership',
+        {name:'Audit Report (Certik, Hacken) ',completed:false},
+        {name:'Tier2 Exchange Listing ',completed:false},
+        {name:'Runboji v2 ',completed:false},
+        {name:'Influencer onboarding ',completed:false},
+        {name:'Bojilogy Version Upgrade ',completed:false},
+        {name:'Charity Partnership',completed:false}
       ],
       isPhaseCompleted: false,
     },
     {
       phaseHeader:'Phase 5',
       phaseItem: [
-        'Bojiv2 NFT Collection                                ',
-        'Chinese,Japanese, Russian Language option (Bojilogy) ',
-        'NFT Marketplace ',
-        'BojiMP ',
-        'Bojilogy Version Upgrade ',
-        'Tier 1 Exchange Listing ',
+        {name:'Bojiv2 NFT Collection ',completed:false},
+        {name:'Chinese,Japanese, Russian Language option (Bojilogy) ',completed:false},
+        {name:'NFT Marketplace ',completed:false},
+        {name:'BojiMP ',completed:false},
+        {name:'Bojilogy Version Upgrade ',completed:false},
+        {name:'Tier 1 Exchange Listing ',completed:false},
       ],
       isPhaseCompleted: false,
     },
     {
       phaseHeader:'Phase 6',
       phaseItem: [
-        'NGO inaugural      ',
-        'Metaverse Game ',
-        'Roadmap v2 Release',
+        {name:'NGO inaugural ',completed:false},
+        {name:'Metaverse Game ',completed:false},
+        {name:'Roadmap v2 Release',completed:false},
       ],
       isPhaseCompleted: false,
     },
@@ -135,12 +136,10 @@ const RoadMap = () => {
                   <div className='mgt-timeline-text'>
                     <ul>
                       {
-                        item.isPhaseCompleted === true
-                        ?item.phaseItem.map((item,index)=>(
-                          <li> <del>{item}</del> </li>
-                        ))
-                        :item.phaseItem.map((item,index)=>(
-                          <li> {item} </li>
+                        item.phaseItem.map((item,index)=>(
+                          item.completed === false
+                          ? <li> {item.name} </li>
+                          :<li> <del>{item.name}</del> </li>
                         ))
                       }
                     </ul>
